019063bace4f29a88a4a48c953d249f7d121a5e6
Ssu-Yu Chou <tiffany0520326@gmail.com>
2026-08-02 22:54:40 -0500

    cant move

--- script.js
+++ script.js
@@ -342,3 +342,27 @@ targetVolume
 },100);
 
 }
+
+document.addEventListener("gesturestart",event=>{
+event.preventDefault();
+},{passive:false});
+
+document.addEventListener("gesturechange",event=>{
+event.preventDefault();
+},{passive:false});
+
+document.addEventListener("gestureend",event=>{
+event.preventDefault();
+},{passive:false});
+
+let lastTouchEnd=0;
+
+document.addEventListener("touchend",event=>{
+const now=Date.now();
+
+if(now-lastTouchEnd<=300){
+event.preventDefault();
+}
+
+lastTouchEnd=now;
+},{passive:false});
